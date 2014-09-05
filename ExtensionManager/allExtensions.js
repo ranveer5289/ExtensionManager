d70ce8f69a19205dd52770539ef00eba0641ddc0
Ranveer Raghuwanshi <rraghuwanshi2@sapient.com>
2014-09-05 16:21:45 +0530

    code cleanup

--- ExtensionManager/allExtensions.js
+++ ExtensionManager/allExtensions.js
@@ -3,14 +3,14 @@ function createExtensionUI(extensionListArray){
 	var extensionLength = extensionListArray.length;
 	
 	for(var i=0; i<extensionLength; i++){
-		
+	
 		var extensionObject = extensionListArray[i];
 		if(extensionObject.type == "extension"){	
 			var checkbox = "";
 			if(extensionObject.enabled){
-					checkbox = '<label><input type="checkbox" checked="checked" class= "extensionboxEnabled" id= "' + extensionObject.id  + '"/>'+extensionObject.shortName+'</label><hr>';
+				checkbox = '<label><input type="checkbox" checked="checked" class= "extensionboxEnabled" id= "' + extensionObject.id  + '"/>'+extensionObject.shortName+'</label><hr>';
 			}else{
-					checkbox = '<label><input type="checkbox" class= "extensionboxDisabled" id= "' + extensionObject.id  + '"/>'+extensionObject.shortName+'</label><hr>';
+				checkbox = '<label><input type="checkbox" class= "extensionboxDisabled" id= "' + extensionObject.id  + '"/>'+extensionObject.shortName+'</label><hr>';
 			}
 			$('#extensionList').append(checkbox);
 			$('#extensionList').append('<div class="clear"></div>');
@@ -22,7 +22,6 @@ function createExtensionUI(extensionListArray){
 function getExtensionList(extensionListArray){
 
 	createExtensionUI(extensionListArray);
-	
 	$("input[type=checkbox]").change(function() {
 			var extensionId = this.id;
 			changeExtensionState(extensionId);
@@ -37,7 +36,6 @@ function changeExtensionState(extensionId){
 
 			var state = extensionInfo.enabled;
 			extensionState = state;			
-			
 			if(extensionState){
 				chrome.management.setEnabled(extensionId, false);
 				$('#status').text("Extension Disabled");
@@ -48,17 +46,6 @@ function changeExtensionState(extensionId){
 	});
 	
 }
-
-function getExtensionStatus(extensionId){
-
-	chrome.management.get(extensionId, function(extensionInfo){
-
-			var state = extensionInfo.enabled;
-			return state;
-	});
-}
-
-
 //Execute the function after page load.
 $(document).ready(function(){
 	chrome.management.getAll(getExtensionList);
